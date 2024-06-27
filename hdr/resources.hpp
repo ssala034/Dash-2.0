44ae2f09f869f2533b897dcb8fc0c50874f7a9d4
ssala034 <sh.salat@gmail.com>
2024-06-27 16:44:33 -0400

    building layout as before

---
+++ hdr/resources.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+//Dependencies
+#include <fstream> // for file input and output operations
+#include <SFML/Graphics.hpp> // a multimedia library for C++ with bindings available for other languages such as Python, helps with 2d graphics
+#include <SFML/Windows.hpp> // helps with windows api
